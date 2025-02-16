3bbd2d9e1627e7a9ea92f1d4964a3b6268b60b58
pmvroque15 <pmvroque@gmail.com>
2025-02-15 22:05:29 -0500

    Initialized buttons, now working on the body of the calculator.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
-const App = () => {
-  return 
+function App() {
+  return <div>
   <div className="calculator-grid">
     <div className="output">
       <div className="previous-operand">
@@ -9,6 +9,25 @@ const App = () => {
         </div>
       </div>
     </div>
+    <button className="span-two">AC</button>
+    <button>DEL</button>
+    <button>÷</button>
+    <button>1</button>
+    <button>2</button>
+    <button>3</button>
+    <button>*</button>
+    <button>4</button>
+    <button>5</button>
+    <button>6</button>
+    <button>+</button>
+    <button>7</button>
+    <button>8</button>
+    <button>9</button>
+    <button>-</button>
+    <button>.</button>
+    <button>0</button>
+    <button className="span-two">=</button>
+  </div>
   </div>
 }
 
